b3ed11283362ad319e2aa5b175254735810b56e6
DcVillamil <dcvillamil9466@gmail.com>
2022-02-19 10:29:34 -0500

    seccion contacto, links y footer

--- index.php
+++ index.php
@@ -1,5 +1,90 @@
 <?php include_once __DIR__."/header.html";?>
+<link rel="stylesheet" href="./style.css">
 
 <!-- <h1 class="bg-dark">hola</h1> -->
+
+<section class="contacto">
+    <div class="container">
+      <div class="row">
+        <div class="container">
+          <div class="contenedor-titulo badge bg-light">
+          <h4 class="text-center"><strong>ÚNETE A LA CONVERSACIÓN</strong></h4>
+          </div>
+        </div>
+        </div>
+
+      <div class="container row info">
+        <h5 class="contactobox text-center"> ¿Tienes inquietudes sobre tu Mazda o del servicio recibido?
+       <a href="#">Ingresa aquí</a>
+        <br>
+        <br>
+        Mazda de Colombia S.A.S.
+        <br>
+        <span>Dirección: Carrera 7 No.75-66 / Bogotá - Colombia / Teléfono:  (1) 2 35 75 29 Línea Nacional: 01 8000 112 595</span></h5>
+      </div>
+    </div>
+  </section>
+
+
+  <section class="link-contacto">
+    <div class="container">
+     <div class="row link">
+          <div class="col orden" >
+          <button class="btn btn-primary" style="margin-bottom:10px;" >Noticias</button>
+          <br>
+          <button class="btn btn-primary" style="margin-bottom:10px;">Videos</button>
+          </div>
+           <div class="col orden2">
+            <button class="btn btn-primary"style="margin-bottom:10px;" >Ventas Corporativas</button>
+            <br>
+          <button class="btn btn-primary"style="margin-bottom:10px;" >Contáctanos</button>
+          </div>
+           <div class="col">
+            <div class="w-50 p-3" style="background-color: #eee;">
+              <a href="#">Mazda CX-3, con los mejores costos de reparación de su categoría </a>
+              <br>
+              <p>• Mazda CX- 3, en su versión Prime, se des...</p><a href="#">Más información</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+  </section>
+
+  <section class="final">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <ul class="nav justify-content-center">
+            <li class="nav-item">
+              <a class="nav-link active" href="#">Catálogos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Concesionarios</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Aviso Legal</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#">Mapa del Sitio</a>
+            </li>
+          </ul>
+
+          </div>
+        <div class="col">
+          <div class="row svg">
+            <div class="col">
+               <p class="siguenos">Siguenos: </p>
+            </div>
+            <div class="col-9">
+               <img style="height:30px;width: 30px;" src="https://i.imgur.com/deZv7DO.png">
+            </div>
+           
+          </div>
+        </div>
+      </div>
+    </div>
+    
+  </section>
 
 <?php include_once __DIR__."/footer.html";?>
